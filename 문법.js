06eba8c0bf1f5ff01d5c1e1175e66ba6713f98be
Choi Wonseo <cws4725@gmail.com>
2023-03-15 10:03:52 +0900

    화살표 함수 줄이기

--- 문법.js
+++ 문법.js
@@ -27,9 +27,7 @@ result = sub(3, 2);
 console.log(result); // 1
 
 // 화살표 함수로 변형
-sub = (n1, n2) => {
-  return n1 - n2;
-}
+sub = (n1, n2) => n1 - n2
 result = sub(3, 2);
 console.log(result); //1
 
