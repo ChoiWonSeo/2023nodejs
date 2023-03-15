82eb57339cdab66930d4c550d78dbccd6aa71e66
Choi Wonseo <cws4725@gmail.com>
2023-03-15 09:53:35 +0900

    함수 기본 사용법

--- 문법.js
+++ 문법.js
@@ -5,4 +5,23 @@
 
 const a = 30;
 console.log(a);
-//a = 10;
+// a = 10;
+
+
+// 함수
+function add(n1, n2) {
+  return n1 + n2;
+}
+
+result = add(1, 2);
+console.log(result); // 3
+
+plus = add; // plus도 add()의 역할을 할 수 있음.
+result = plus(3, 4);
+console.log(result); // 7
+
+sub = function(n1, n2) {
+  return n1 - n2;
+}
+result = sub(3, 2);
+console.log(result); // 1
