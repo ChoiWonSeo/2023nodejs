aefe51678370ce2966d7d63d41b12884aa81825b
Choi Wonseo <cws4725@gmail.com>
2023-03-16 14:04:31 +0900

    object cathy

---
+++ object.js
@@ -0,0 +1,15 @@
+const cathy = {
+  "name" : "cathy",
+  "age" : 3,
+  "skills" : ["자바스크립트", "인공지능", "자바", "리액트", "코볼"]
+};
+
+console.log(cathy);
+console.log(cathy.name);
+
+cathy["name"] = "용복이";
+console.log(cathy["name"]);
+
+cathy["성별"] = "man";
+console.log(cathy.성별);
+
