8ec055cff868d80bd110953725de9c7630fe2133
Choi Wonseo <cws4725@gmail.com>
2023-04-12 10:08:47 +0900

    array

--- 문법/array.js
+++ 문법/array.js
@@ -3,3 +3,6 @@ const arr = ['apple', 'banana', 'cherry', 'apple'];
 arr.forEach(function(data, index) {
   console.log(`${index} : ${data}`);
 });
+
+const cIndex = arr.findIndex(data => data === 'cherry')
+console.log(cIndex);
