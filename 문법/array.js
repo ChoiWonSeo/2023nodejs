983dee9f4b74691ab2151c45f93cf28f7bdfef97
Choi Wonseo <cws4725@gmail.com>
2023-04-12 10:17:19 +0900

    array 중간

--- 문법/array.js
+++ 문법/array.js
@@ -4,5 +4,14 @@ arr.forEach(function(data, index) {
   console.log(`${index} : ${data}`);
 });
 
-const cIndex = arr.findIndex(data => data === 'cherry')
-console.log(cIndex);
+// const cIndex = arr.findIndex(data => data === 'cherry')
+const a = arr.indexOf('cherry', 3);
+console.log(a);
+// console.log(cIndex);
+
+arr.push('grape');
+// console.log(arr);
+
+arr.pop();
+console.log(x);
+console.log(arr);
